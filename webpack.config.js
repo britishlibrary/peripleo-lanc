a2d3066c972f6d06dc599347d0f69edfb230743c
Rainer Simon <aboutgeo@gmail.com>
2022-02-14 14:32:11 +0100

    Basic dev dependencies + Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    filename: 'peripleo-lanc.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'mapbox-gl': 'maplibre-gl'
+    }
+  },
+  optimization: {
+    minimize: true
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.(js|jsx)$/, 
+        exclude: [/node_modules/],
+        use: { 
+          loader: 'babel-loader' ,
+          options: {
+            "presets": [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ],
+            "plugins": [
+              [
+                "@babel/plugin-proposal-class-properties"
+              ]
+            ]
+          }
+        }
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.s[ac]ss$/i, use: ['style-loader', 'css-loader', 'sass-loader' ] }
+    ]
+  },
+  experiments: {
+    asyncWebAssembly: true
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    compress: true,
+    hot: true,
+    host: process.env.HOST || 'localhost',
+    port: 3000,
+    static: {
+      directory: './public',
+      publicPath: '/'
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: 'head',
+      template: './public/index.html'
+    })
+  ]
+};
