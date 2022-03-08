aa61948281c7fd9248a5f5a06fd567fce4bd6723
Rainer Simon <aboutgeo@gmail.com>
2022-03-08 15:08:57 +0100

    Changed color palette

--- src/Colors.js
+++ src/Colors.js
@@ -1,3 +1,4 @@
+/*
 export const SIGNATURE_COLOR = [
   '#0072dc', // Blue
   '#009f29', // Green
@@ -7,4 +8,18 @@ export const SIGNATURE_COLOR = [
   '#c48000', // Brown
   '#0eaec8', // Cyan
   '#5c5c5c'  // Grey
+];
+
+export const PALETTE_ISABELLA = [
+*/
+
+export const SIGNATURE_COLOR = [
+  '#ffd300', // Yellow
+  '#0d8b22', // Green
+  '#0e47c8', // Dark blue
+  '#0095ff', // Light blue
+  '#9d00d1', // Purple
+  '#ff5bdb', // Pink
+  '#ff2e38', // Red
+  '#ff8000'  // Orange
 ];
